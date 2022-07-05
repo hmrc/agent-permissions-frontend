622130b4da6e3e33a43cb8b513f0fcef0c9bfae8
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2022-07-05 16:49:34 +0100

    APB-6166 update back link on create group

--- test/controllers/GroupControllerSpec.scala
+++ test/controllers/GroupControllerSpec.scala
@@ -147,6 +147,10 @@ class GroupControllerSpec extends BaseSpec {
 
       val html = Jsoup.parse(contentAsString(result))
       html.title() shouldBe "Create an access group - Manage Agent Permissions - GOV.UK"
+      html
+        .select(Css.backLink)
+        .attr("href") shouldBe "http://localhost:9401/agent-services-account/manage-account"
+      html.select(Css.backLink).text() shouldBe "Back"
       html.select(Css.H1).text() shouldBe "Create an access group"
       html
         .select(Css.form)

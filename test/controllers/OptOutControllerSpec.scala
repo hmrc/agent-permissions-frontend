bbbcb92c076bc3fbbe16ee1bb91721603fe66daf
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2022-09-13 15:25:19 +0100

    a11y [CS] update error messages for opt in/out

--- test/controllers/OptOutControllerSpec.scala
+++ test/controllers/OptOutControllerSpec.scala
@@ -184,10 +184,10 @@ class OptOutControllerSpec extends BaseSpec {
       html.title() shouldBe "Error: Do you want to turn off access groups? - Agent services account - GOV.UK"
       html
         .select(Css.errorSummaryForField("answer"))
-        .text() shouldBe "Please select an option."
+        .text() shouldBe "Select yes if you want to turn access groups off"
       html
         .select(Css.errorForField("answer"))
-        .text() shouldBe "Error: Please select an option."
+        .text() shouldBe "Error: Select yes if you want to turn access groups off"
 
       html.select(Css.submitButton)
 

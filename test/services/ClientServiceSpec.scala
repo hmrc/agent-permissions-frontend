95146f3aaf30350dbb6d13e88290725dde88822a
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2022-10-31 16:58:12 +0000

    APB-6605 fix manage group; clients/team members saving before confirmation

--- test/services/ClientServiceSpec.scala
+++ test/services/ClientServiceSpec.scala
@@ -269,19 +269,21 @@ class ClientServiceSpec extends BaseSpec {
       await(service.addSelectablesToSession(displayClients.take(3).toList)(SELECTED_CLIENTS, FILTERED_CLIENTS))
     }
     
-    "work as expected with SELECTED_CLIENTS & FILTERED_CLIENTS in session " in {
+    "work as expected with selected in session " in{
       //TODO: don't think this is the desired behaviour
       //given existing session state
       expectGetSessionItem(SELECTED_CLIENTS, displayClients.take(2).map(_.copy(selected = true)))
       expectGetSessionItem(FILTERED_CLIENTS, displayClients.takeRight(1).map(_.copy(selected = true)))
 
-      val expectedToSaveInSession = List(
+      val expectedPayload = List(
+        DisplayClient("123456781","friendly name 1","HMRC-MTD-VAT","VRN",false),
+        DisplayClient("123456782","friendly name 2","HMRC-MTD-VAT","VRN",false),
+        DisplayClient("123456783","friendly name 3","HMRC-MTD-VAT","VRN",false),
         DisplayClient("123456781","friendly name 1","HMRC-MTD-VAT","VRN",true),
-        DisplayClient("123456782","friendly name 2","HMRC-MTD-VAT","VRN",true),
-        DisplayClient("123456783","friendly name 3","HMRC-MTD-VAT","VRN",true),
+        DisplayClient("123456782","friendly name 2","HMRC-MTD-VAT","VRN",true)
       )
-      //we expect the session to be changed like this
-      expectPutSessionItem(SELECTED_CLIENTS, expectedToSaveInSession)
+      //we expect the sesion to be changed like this
+      expectPutSessionItem(SELECTED_CLIENTS, expectedPayload)
 
       //when
       await(service.addSelectablesToSession(displayClients.take(3).toList)(SELECTED_CLIENTS, FILTERED_CLIENTS))

251cebc0fb57f939bc6016da6a31b9e06175dcb9
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2022-11-04 14:51:19 +0000

    APB-6605  fix client search

--- app/controllers/CreateGroupController.scala
+++ app/controllers/CreateGroupController.scala
@@ -146,13 +146,7 @@ class CreateGroupController @Inject()(
       withSessionItem[String](CLIENT_FILTER_INPUT) { clientFilterTerm =>
         withSessionItem[String](CLIENT_SEARCH_INPUT) { clientSearchTerm =>
           withSessionItem[String](RETURN_URL) { returnUrl =>
-            println("***************")
-            println(clientSearchTerm)
-            println("***************")
             clientService.getFilteredClientsElseAll(arn).map { clients =>
-              println("***************")
-              println(clients)
-              println("***************")
               Ok(
                 client_group_list(
                   clients,

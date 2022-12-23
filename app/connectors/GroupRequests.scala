8dab702506c3b1d2a6a52410c378f885bf40e8f4
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2022-12-23 12:35:09 +0000

    APB-6718 create Tax Service Group and confirmation

---
+++ app/connectors/GroupRequests.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import play.api.libs.json.{Json, OFormat}
+import uk.gov.hmrc.agentmtdidentifiers.model.{AgentUser, Client}
+
+case class GroupRequest(groupName: String,
+                        teamMembers: Option[Seq[AgentUser]],
+                        clients: Option[Seq[Client]])
+
+case object GroupRequest {
+  implicit val formatCreateAccessGroupRequest: OFormat[GroupRequest] =
+    Json.format[GroupRequest]
+}
+
+case class GroupSummary(groupId: String,
+                        groupName: String,
+                        clientCount: Option[Int],
+                        teamMemberCount: Int,
+                        isCustomGroup: Boolean)
+
+case object GroupSummary {
+  implicit val formatCreateAccessGroupRequest: OFormat[GroupSummary] =
+    Json.format[GroupSummary]
+}
+
+case class UpdateAccessGroupRequest(
+                                     groupName: Option[String] = None,
+                                     teamMembers: Option[Set[AgentUser]] = None,
+                                     clients: Option[Set[Client]] = None
+                                   )
+
+object UpdateAccessGroupRequest {
+  implicit val format: OFormat[UpdateAccessGroupRequest] =
+    Json.format[UpdateAccessGroupRequest]
+}
+
+case class AddMembersToAccessGroupRequest(
+                                           teamMembers: Option[Set[AgentUser]] = None,
+                                           clients: Option[Set[Client]] = None
+                                         )
+
+object AddMembersToAccessGroupRequest {
+  implicit val format: OFormat[AddMembersToAccessGroupRequest] = Json.format[AddMembersToAccessGroupRequest]
+}

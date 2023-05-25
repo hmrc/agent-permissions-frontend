618fe424bb430122ef5320ad7fdfa64d6e32e199
Max Ashton <68871949+max-hmrc@users.noreply.github.com>
2023-05-25 12:12:52 +0100

    APB-7229 migrate to play frontend 7 (#382)
    * APB-7229 migrate to play frontend 7
    
    * APB-7229 migrate to play frontend 7

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-28"     % bootstrapVer,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc"             % "5.5.0-play-28",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"             % mongoVer,
+    "uk.gov.hmrc"        %% "play-frontend-hmrc"             % "7.7.0-play-28",
+    "uk.gov.hmrc"        %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"          % "1.2.0",
     "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"       % "5.3.0",
-    "uk.gov.hmrc"        %% "play-conditional-form-mapping"  % "1.12.0-play-28"
   )
 
   val test = Seq(
@@ -19,6 +19,6 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % mongoVer   % "test, it",
     "org.jsoup"               %  "jsoup"                      % "1.13.1"   % "test, it",
     "org.scalamock"           %% "scalamock"                  % "5.2.0"    % "test, it",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"   % "test, it"
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"   % "test, it",
   )
 }

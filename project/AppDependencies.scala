3bf4b2c36eb1ae792886c9d4616c471e404e334d
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-07-10 16:18:47 +0100

    APB-9678 fix test and dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"            % bootstrapVer,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                    % mongoVer,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"            % "12.6.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"            % "12.7.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"                 % "2.2.0",
     "uk.gov.hmrc"        %% "crypto-json-play-30"                   % "8.2.0"

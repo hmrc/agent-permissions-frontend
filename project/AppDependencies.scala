1f6ef7f8e08966b34482c39bdc32cc6bae6ad4b5
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-12-19 15:59:26 +0000

    APB-10234: Addresses PlatopsPRBot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVer,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVer,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.24.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0"

1fe0f0237e6b216a6e57f0bae8c164c71d1d9fd7
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-01-02 15:15:47 +0000

    APB-10234: Upgrades play-frontend-hmrc to 12.25.0 to test build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVer"            % bootstrapVer,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVer"                    % mongoVer,
 //    TODO: Investigate version upgrade wrt build
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVer"            % "12.10.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVer"            % "12.25.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVer" % "3.4.0",
     "uk.gov.hmrc"       %% s"crypto-json-$playVer"                   % "8.4.0",
     "uk.gov.hmrc"       %% s"domain-$playVer"                        % "11.0.0"

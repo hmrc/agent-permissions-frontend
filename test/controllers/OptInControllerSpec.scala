7f136c6bff3b4809c9144e7b6ff5cc1369b2a5b7
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-06-17 11:45:09 +0100

    APB-11676: Updates phase banner content check in OptInControllerSpec

--- test/controllers/OptInControllerSpec.scala
+++ test/controllers/OptInControllerSpec.scala
@@ -94,7 +94,7 @@ class OptInControllerSpec extends BaseSpec {
 
       val phaseBanner = html.select("div.govuk-phase-banner")
       phaseBanner.isEmpty shouldBe false
-      phaseBanner.select("a").text shouldBe "give your feedback by email"
+      phaseBanner.select("a").text shouldBe "give your feedback (opens in new tab)"
       phaseBanner.select("a").attr("href") shouldBe "http://localhost:9250/contact/beta-feedback?service=AOSS"
 
       html.select(Css.backLink).attr("href") shouldBe "http://localhost:9401/agent-services-account/manage-account"

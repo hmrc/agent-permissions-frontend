3a90f8f69a96f6e7adf43e4924547a6e6c379823
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-06-17 11:28:17 +0100

    APB-11676: Updates play-frontend-hmrc to highest v12 possible

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVer"            % bootstrapVer,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVer"                    % mongoVer,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVer"            % "12.27.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVer"            % "12.32.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVer" % "3.4.0",
     "uk.gov.hmrc"       %% s"crypto-json-$playVer"                   % "8.4.0",
     "uk.gov.hmrc"       %% s"domain-$playVer"                        % "11.0.0"
